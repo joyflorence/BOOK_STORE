a7480fd624729932d727be915edba21207b93c10
Joy <aworjoy8@gmail.com>
2024-04-05 10:48:33 +0300

    deleted components

--- onlinebookstore/src/App.js
+++ onlinebookstore/src/App.js
@@ -1,25 +1,10 @@
 import React from 'react'
-import {BrowserRouter as Router,Route, Routes} from 'react-router-dom'
-import BookCatalog from './component/catalog/BookCatalog';
-import ShoppingCart from './component/cart/ShoppingCart';
-import UserProfile from './component/profile/UserProfile';
-import AdminPanel from './component/Admin/AdminPanel';
-import Home from './component/pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-      
-     <Router>
-      <Routes>
-     
-        <Route path='/' element={<Home/>}/>
-        <Route path='/books' element={ <BookCatalog />}/>
-        <Route path='/profile' element={<UserProfile />}/>
-        <Route path='/cart' element={<ShoppingCart />}/>
-        <Route path='/admin' element={ <AdminPanel />}/>
-      </Routes>
-     </Router>
+      <h1>Hello world</h1>
     </div>
   );
 }
